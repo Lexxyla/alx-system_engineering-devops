0a6aada22bef0e94222aab34e19d2fbcb248546a
lexxyla <bamideleadefolaju@gmail.com>
2022-11-01 22:38:49 +0100

    update

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
-''' task 2 module'''
-
+"""Contains the count_words function"""
 import requests
 
 
-def count_words(subreddit, word_list):
-    '''return count of keywords in hot posts titles'''
+def count_words(subreddit, word_list, found_list=[], after=None):
+    '''Prints counts of given words found in hot posts of a given subreddit.
+
+    Args:
+        subreddit (str): The subreddit to search.
+        word_list (list): The list of words to search for in post titles.
+        found_list (obj): Key/value pairs of words/counts.
+        after (str): The parameter for the next page of the API results.
+    '''
     user_agent = {'User-agent': 'test45'}
     posts = requests.get('http://www.reddit.com/r/{}/hot.json?after={}'
                          .format(subreddit, after), headers=user_agent)
